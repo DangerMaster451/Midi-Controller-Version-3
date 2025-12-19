690e82cf158697393038a5b1f5fbdffde03cbf03
Andrew <andrewrpelton@outlook.com>
2025-12-18 23:25:35 -0800

    added the timer logic

--- MidiActions.py
+++ MidiActions.py
@@ -1,5 +1,9 @@
 import tkinter as tk
 from tkinter import ttk, Toplevel
+import random
+from pygame import mixer
+
+mixer.init()
 
 class MidiEvent():
     def __init__(self, event) -> None:
@@ -8,11 +12,11 @@ class MidiEvent():
         self.velocity = event[0][2]
 
 class MidiAction():
-    def __init__(self, name:str, allowed_midi_statuses:list[int]|None = None, allowed_midi_notes:list[int]|None = None, allowed_midi_velocities:list[int]|None = None):
+    def __init__(self, name:str) -> None:
         self.name = name
-        self.allowed_midi_statuses:list[int]|None = allowed_midi_statuses
-        self.allowed_midi_notes:list[int]|None = allowed_midi_notes
-        self.allowed_midi_velocities:list[int]|None = allowed_midi_velocities
+        self.allowed_midi_statuses:list[int]|None
+        self.allowed_midi_notes:list[int]|None
+        self.allowed_midi_velocities:list[int]|None
 
     def verify(self, event:MidiEvent) -> bool:
         if self.allowed_midi_statuses != None and event.status not in self.allowed_midi_statuses:
@@ -23,21 +27,28 @@ class MidiAction():
             return False
         return True
 
-    def action(self, event:MidiEvent):
+    def action(self, event:MidiEvent) -> None:
         pass
 
 class Timer(MidiAction):
-    def __init__(self, window:tk.Tk):
-        self.name = "timer"
-        super().__init__(self.name)
+    def __init__(self, window:tk.Tk) -> None:
+        super().__init__("timer")
         
         self.allowed_midi_statuses:list[int]|None = [176]
-        self.allowed_midi_notes:list[int]|None = [74]
+        self.allowed_midi_notes:list[int]|None = [74, 118]
         self.allowed_midi_velocities:list[int]|None = None
+
+        self.maxTime = 90
+
+        self.timeValue:int = 45
+        self.timerRunning:bool = False
+
+        self.__EASAlarm = mixer.Sound("sounds/EAS.mp3")
+        self.__normalAlarm = mixer.Sound("sounds/lofiAlarm.mp3")
 
         self.window = tk.Toplevel(window)
         self.window.title("Timer")
-        #self.window.withdraw()
+        self.window.withdraw()
 
         self.label = tk.Label(self.window, text="Timer", anchor=tk.CENTER)
         self.label.pack(pady=10)
@@ -46,7 +57,62 @@ class Timer(MidiAction):
         self.progressbar["maximum"] = 90
         self.progressbar["value"] = 45
         self.progressbar.pack(padx=20, pady=20)
+
+        self.window.protocol("WM_DELETE_WINDOW", self.window.withdraw)
     
-    def action(self, event:MidiEvent):
-        self.progressbar["value"] = event.velocity
-        
+    def action(self, event:MidiEvent) -> None:
+        if event.note == 74:
+            self.__handleKnob(event)
+        if event.note == 118:
+            self.__handleButton(event)
+    
+    def __handleKnob(self, event:MidiEvent) -> None:
+        self.window.deiconify()
+        if self.timerRunning:
+            return None
+        self.timeValue = self.__midiVelocityToTimeValue(event.velocity)
+        self.progressbar["value"] = self.timeValue
+        self.label["text"] = self.__convertTimeValueToString(self.timeValue)
+
+    def __handleButton(self, event:MidiEvent) -> None:
+        self.window.withdraw()
+        if self.timerRunning:
+            return None
+        if self.timeValue == 0:
+            mixer.pause()
+            return None
+        self.timerRunning = True
+        self.progressbar["maximum"] = self.timeValue
+        self.window.after(0, self.__timerTick)
+
+    def __convertTimeValueToString(self, velocity:int) -> str:
+        if velocity >= 60:
+            if velocity-60 >= 10:
+                return f"01:{velocity-60}"
+            return f"01:0{velocity-60}"
+        if velocity >= 10:
+            return f"00:{velocity}"
+        return f"00:0{velocity}"
+    
+    def __midiVelocityToTimeValue(self, velocity:int) -> int:
+        return round(velocity / 127 * self.maxTime)
+    
+    def __timerTick(self) -> None:
+        self.timeValue -= 1
+        self.progressbar["value"] = self.timeValue
+        self.label["text"] = self.__convertTimeValueToString(self.timeValue)
+        self.window.update_idletasks()
+
+        if self.timeValue > 0:
+            self.window.after(60000, self.__timerTick)
+        else:
+            self.timerRunning = False
+            self.progressbar["maximum"] = 90
+            self.__timerSound()
+
+    def __timerSound(self) -> None:
+        self.window.deiconify()
+        if random.randint(1,6) == 1:
+            self.__EASAlarm.play()
+        else:
+            self.__normalAlarm.play()
