0a6925b4048b119ec16e5591eae7d72ebc9086da
Andrew <andrewrpelton@outlook.com>
2025-12-24 22:37:36 -0800

    Added the client for controlling a separate device

--- Midi/MediaController.py
+++ Midi/MediaController.py
@@ -1,80 +1,23 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
 import tkinter as tk
-import threading
+import requests
 import Midi
+
 
 class MediaController(Midi.MidiAction):
     def __init__(self, window:tk.Tk) -> None:
-        super().__init__("Media Controller")
+        super().__init__("MediaController")
         
-        self.allowed_midi_statuses:list[int]|None = [176]
-        self.allowed_midi_notes:list[int]|None = [64]
-        self.allowed_midi_velocities:list[int]|None = [0]
+        self.allowed_midi_statuses:list[int]|None = [176, 144]
+        self.allowed_midi_notes:list[int]|None = [64, 49, 51]
+        self.allowed_midi_velocities:list[int]|None = None
 
-        self.__window = tk.Toplevel(window)
-        self.__window.title("Media Controller")
-        self.__window.resizable(False, False)
-        self.__window.withdraw()
+        self.window = window
 
-        self.__window.protocol("WM_DELETE_WINDOW", self.__window.withdraw)
-
-        self.currentCommand:str = "None"
-
-        server = Server("", 8000, self)
-        server.start()
-        print("Server Started!")
-
+       
     def action(self, event:Midi.MidiEvent) -> None:
-        if self.currentCommand == "None":
-            self.currentCommand = "toggle"
-        
-class Server(threading.Thread):
-    def __init__(self, host, port, controller:MediaController):
-        threading.Thread.__init__(self)
-        self.host = host
-        self.port = port
-        self.daemon = True
-        self.controller = controller
-        
-    def run(self):
-        def handler(*args):
-            return Handler(self.controller, *args)
-        server = HTTPServer((self.host, self.port), handler)
-        server.serve_forever()
-
-class Handler(BaseHTTPRequestHandler):
-    def __init__(self, controller:MediaController, *args):
-        self.controller = controller
-        BaseHTTPRequestHandler.__init__(self, *args)
-
-    def log_message(self, format: str, *args) -> None:
-        pass
-
-    def do_GET(self):
-        if self.path == "/controller":
-            self.send_response(200)
-            self.send_header("Contest-type", "text/html")
-            self.end_headers()
-
-            with open("html/controller.html", "rb") as file:
-                content = file.read()
-                self.wfile.write(content)
-
-
-        elif self.path == "/clicked?":
-            self.send_response(200)
-            self.send_header("Contest-type", "text/html")
-            self.end_headers()
-
-            with open("html/clicked.html", "rb") as file:
-                content = file.read()
-                self.wfile.write(content)
-            self.controller.currentCommand = "toggle"
-
-        else:
-            self.send_response(200)
-            self.send_header("Contest-type", "text/plain")
-            self.end_headers()
-
-            self.wfile.write(self.controller.currentCommand.encode("utf-8"))
-            self.controller.currentCommand = "None"
+        if event.status == 176 and event.note == 64 and event.velocity == 0:
+            r = requests.get("http://192.168.4.205:8000/togglePlayback")
+        elif event.status == 144 and event.note == 49:
+            r = requests.get("http://192.168.4.205:8000/prevTab")
+        elif event.status == 144 and event.note == 51:
+            r = requests.get("http://192.168.4.205:8000/nextTab")
